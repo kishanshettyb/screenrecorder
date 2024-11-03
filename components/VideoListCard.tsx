cc00f9f1bafc18bf8aadd4765db5a89b070e832f
kishanshettyb <kishanqr@gmail.com>
2024-11-04 03:17:55 +0530

    feat: :lipstick: update styles for component logo and video list card

--- components/VideoListCard.tsx
+++ components/VideoListCard.tsx
@@ -1,7 +1,8 @@
 import React, { FunctionComponent } from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import Feather from "@expo/vector-icons/Feather";
 import { useColorScheme } from "nativewind";
+import { Link } from "expo-router";
 
 const VideoListCard: FunctionComponent = () => {
   const { colorScheme } = useColorScheme();
@@ -23,69 +24,71 @@ const VideoListCard: FunctionComponent = () => {
       <Text className="text-xs mb-2 text-neutral-900 dark:text-neutral-400 opacity-40">
         Today
       </Text>
-      <View className="mb-3 rounded-2xl flex flex-row justify-between items-center gap-x-4 p-4 border-slate-100  dark:border-neutral-800 border bg-white dark:bg-neutral-950 shadow w-full">
-        <View className="w-[90px] h-[100px] rounded-xl  relative  ">
-          <Image
-            className="w-[90px] h-[100px] rounded-xl"
-            source={require("../assets/images/video-sample.jpg")}
-          />
-          <View className="absolute left-0 top-0 w-full h-full flex items-center justify-center">
-            <Feather name="play" size={24} color="white" />
-          </View>
-        </View>
-        <View className="flex flex-1">
-          <Text numberOfLines={2} className="mb-2  text-neutral-500">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
-            perspiciatis adipisci voluptatibus ex reiciendis maxime provident
-            odit. Suscipit tempora iusto natus, odit doloremque similique facere
-            facilis aut non, voluptas eos?
-          </Text>
-          <View className="flex flex-row justify-start items-center gap-x-1 mb-3 opacity-50">
-            <View>
-              <Feather
-                name="calendar"
-                size={10}
-                color={colorScheme === "dark" ? "#71717a" : "#171717"}
-              />
-            </View>
-            <View>
-              <Text className="text-xs text-neutral-900 dark:text-neutral-500">
-                02-11-2024
-              </Text>
+      <Link asChild href="/(screens)/player">
+        <TouchableOpacity className="mb-3 rounded-2xl flex flex-row justify-between items-center gap-x-4 p-4 border-slate-100  dark:border-neutral-800 border bg-white dark:bg-neutral-950 shadow w-full">
+          <View className="w-[90px] h-[100px] rounded-xl  relative  ">
+            <Image
+              className="w-[90px] h-[100px] rounded-xl"
+              source={require("../assets/images/video-sample.jpg")}
+            />
+            <View className="absolute left-0 top-0 w-full h-full flex items-center justify-center">
+              <Feather name="play" size={24} color="white" />
             </View>
           </View>
-          <View className="flex justify-between border rounded-xl p-2 border-slate-100 dark:border-neutral-900 flex-row items-center">
-            <View>
-              <Text className="text-neutral-900 dark:text-neutral-500 opacity-60">
-                10.02 MB
-              </Text>
-            </View>
-            <View className="flex flex-row  justify-between  gap-x-6 opacity-30">
+          <View className="flex flex-1">
+            <Text numberOfLines={2} className="mb-2  text-neutral-500">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
+              perspiciatis adipisci voluptatibus ex reiciendis maxime provident
+              odit. Suscipit tempora iusto natus, odit doloremque similique
+              facere facilis aut non, voluptas eos?
+            </Text>
+            <View className="flex flex-row justify-start items-center gap-x-1 mb-3 opacity-50">
               <View>
                 <Feather
-                  name="edit-2"
-                  size={17}
+                  name="calendar"
+                  size={10}
                   color={colorScheme === "dark" ? "#71717a" : "#171717"}
                 />
               </View>
               <View>
-                <Feather
-                  name="share-2"
-                  size={17}
-                  color={colorScheme === "dark" ? "#71717a" : "#171717"}
-                />
+                <Text className="text-xs text-neutral-900 dark:text-neutral-500">
+                  02-11-2024
+                </Text>
               </View>
+            </View>
+            <View className="flex justify-between border rounded-xl p-2 border-slate-100 dark:border-neutral-900 flex-row items-center">
               <View>
-                <Feather
-                  name="more-vertical"
-                  size={17}
-                  color={colorScheme === "dark" ? "#71717a" : "#171717"}
-                />
+                <Text className="text-neutral-900 dark:text-neutral-500 opacity-60">
+                  10.02 MB
+                </Text>
+              </View>
+              <View className="flex flex-row  justify-between  gap-x-6 opacity-30">
+                <View>
+                  <Feather
+                    name="edit-2"
+                    size={17}
+                    color={colorScheme === "dark" ? "#71717a" : "#171717"}
+                  />
+                </View>
+                <View>
+                  <Feather
+                    name="share-2"
+                    size={17}
+                    color={colorScheme === "dark" ? "#71717a" : "#171717"}
+                  />
+                </View>
+                <View>
+                  <Feather
+                    name="more-vertical"
+                    size={17}
+                    color={colorScheme === "dark" ? "#71717a" : "#171717"}
+                  />
+                </View>
               </View>
             </View>
           </View>
-        </View>
-      </View>
+        </TouchableOpacity>
+      </Link>
       <Text className="text-xs mb-2 text-neutral-900 dark:text-neutral-400 opacity-40">
         Yesterday
       </Text>
